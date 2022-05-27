77210ec99fb36d844c2030cf2509faeb05540cba
arvin.zhu <964505072@qq.com>
2022-05-27 21:48:50 +0800

    style: 样式调整

--- src/components/msw-ui/component/addMockPanel.tsx
+++ src/components/msw-ui/component/addMockPanel.tsx
@@ -33,11 +33,12 @@ export const AddMockPanel = observer(
       setPageSize(1);
       store.setFilterKeyword('');
     }, [tableTab]);
+    const shouldNotShowTable =
+      !currentEditGroupRequest && !showConflictDetail && !currentHostSwitch;
     if (
       !unHandleAllRequest?.length &&
       tableTab === 'unHandle' &&
-      !currentEditGroupRequest &&
-      !showConflictDetail
+      shouldNotShowTable
     ) {
       return (
         <div className="msw_handle_noRequest" style={{ paddingTop: 10 }}>
@@ -55,7 +56,11 @@ export const AddMockPanel = observer(
         </div>
       );
     }
-    if (!handledAllRequest?.length && tableTab === 'handled') {
+    if (
+      !handledAllRequest?.length &&
+      tableTab === 'handled' &&
+      shouldNotShowTable
+    ) {
       return (
         <div className="msw_handle_noRequest">
           <div style={{ paddingTop: 10 }}>暂无拦截中的请求</div>
@@ -65,158 +70,152 @@ export const AddMockPanel = observer(
     return (
       <div>
         <div>
-          {!currentEditGroupRequest &&
-            !currentHostSwitch &&
-            !showConflictDetail && (
-              <div style={{ padding: 10 }}>
-                <div className={'msw_addMock_filter'}>
-                  <span>过滤: </span>
-                  <input
-                    value={filterKeywords}
-                    onChange={filterChange}
-                    placeholder={'过滤拦截的请求'}
-                  />
-                </div>
-                <table className="msw_request_table">
-                  <thead>
-                    <tr>
-                      {tableTab !== 'unHandle' && (
-                        <>
-                          <th>模块</th>
-                          <th>组</th>
-                        </>
-                      )}
-                      <th>
-                        {tableTab === 'unHandle'
-                          ? '未拦截的请求'
-                          : '拦截中的请求'}
-                      </th>
-                      <th>操作</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {paginationMock?.[pageSize - 1]?.map(
-                      (im: IGroupDataItem) => {
-                        return (
-                          <tr key={im.request.id + im.collection + im.group}>
-                            {tableTab === 'handled' && (
-                              <>
-                                <td>
-                                  <div
-                                    className="msw_table_module"
-                                    title={im.collection}
-                                  >
-                                    {im.collection}
-                                  </div>
-                                </td>
-                                <td>
-                                  <div
-                                    className="msw_table_module"
-                                    title={im.group}
-                                  >
-                                    {im.group}
-                                  </div>
-                                </td>
-                              </>
-                            )}
+          {shouldNotShowTable && (
+            <div style={{ padding: 10 }}>
+              <div className={'msw_addMock_filter'}>
+                <span>过滤: </span>
+                <input
+                  value={filterKeywords}
+                  onChange={filterChange}
+                  placeholder={'过滤拦截的请求'}
+                />
+              </div>
+              <table className="msw_request_table">
+                <thead>
+                  <tr>
+                    {tableTab !== 'unHandle' && (
+                      <>
+                        <th>模块</th>
+                        <th>组</th>
+                      </>
+                    )}
+                    <th>
+                      {tableTab === 'unHandle'
+                        ? '未拦截的请求'
+                        : '拦截中的请求'}
+                    </th>
+                    <th>操作</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paginationMock?.[pageSize - 1]?.map((im: IGroupDataItem) => {
+                    return (
+                      <tr key={im.request.id + im.collection + im.group}>
+                        {tableTab === 'handled' && (
+                          <>
                             <td>
-                              <span
-                                style={{
-                                  display: 'inline-block',
-                                  width: 50,
-                                  marginRight: 10,
-                                  color: '#F89108'
-                                }}
+                              <div
+                                className="msw_table_module"
+                                title={im.collection}
                               >
-                                {im.request.method}
-                              </span>
-                              <span
-                                className={clsx('msw_table_request', {
-                                  msw_table_module_handle:
-                                    tableTab === 'handled',
-                                  msw_table_module_unHandle:
-                                    tableTab === 'unHandle'
-                                })}
-                                title={im.request.url.href}
-                              >
-                                {im.request.url.href}
-                              </span>
-                            </td>
-                            <td>
-                              <div style={{ textAlign: 'center' }}>
-                                <button
-                                  className="msw_mock_btn small"
-                                  onClick={() => {
-                                    store.setCurrentEditGroupRequest(im);
-                                  }}
-                                >
-                                  {tableTab === 'unHandle' ? 'mock' : '编辑'}
-                                </button>
-                                {tableTab === 'handled' && (
-                                  <Confirm
-                                    content={
-                                      <div style={{ textAlign: 'left' }}>
-                                        确定要删除吗
-                                      </div>
-                                    }
-                                    placement="topRight"
-                                    onOk={() => {
-                                      deleteGroupItem(im);
-                                    }}
-                                  >
-                                    <button className="small msw_mock_btn msw_mock_delete_btn">
-                                      删除
-                                    </button>
-                                  </Confirm>
-                                )}
-                                {tableTab === 'handled' && (
-                                  <button
-                                    className="small msw_mock_btn msw_mock_canCelHandler_btn"
-                                    onClick={() => {
-                                      changeGroupItemStatus(im, false);
-                                    }}
-                                  >
-                                    取消拦截
-                                  </button>
-                                )}
+                                {im.collection}
                               </div>
                             </td>
-                          </tr>
-                        );
-                      }
-                    )}
-                  </tbody>
-                </table>
-                {paginationMock.length > 1 && (
-                  <div className="msw_pagination_wrap">
-                    <button
-                      onClick={() => {
-                        pageSize > 1 && setPageSize(page => page - 1);
-                      }}
-                      disabled={pageSize === 1}
-                      className="small"
-                    >
-                      上一页
-                    </button>
-                    <button
-                      onClick={() => {
-                        pageSize < paginationMock?.length &&
-                          setPageSize(page => page + 1);
-                      }}
-                      disabled={
-                        !(
-                          !!paginationMock?.length &&
-                          pageSize < paginationMock?.length
-                        )
-                      }
-                      className="small"
-                    >
-                      下一页
-                    </button>
-                  </div>
-                )}
-              </div>
-            )}
+                            <td>
+                              <div
+                                className="msw_table_module"
+                                title={im.group}
+                              >
+                                {im.group}
+                              </div>
+                            </td>
+                          </>
+                        )}
+                        <td>
+                          <span
+                            style={{
+                              display: 'inline-block',
+                              width: 50,
+                              marginRight: 10,
+                              color: '#F89108'
+                            }}
+                          >
+                            {im.request.method}
+                          </span>
+                          <span
+                            className={clsx('msw_table_request', {
+                              msw_table_module_handle: tableTab === 'handled',
+                              msw_table_module_unHandle: tableTab === 'unHandle'
+                            })}
+                            title={im.request.url.href}
+                          >
+                            {im.request.url.href}
+                          </span>
+                        </td>
+                        <td>
+                          <div style={{ textAlign: 'center' }}>
+                            <button
+                              className="msw_mock_btn small"
+                              onClick={() => {
+                                store.setCurrentEditGroupRequest(im);
+                              }}
+                            >
+                              {tableTab === 'unHandle' ? 'mock' : '编辑'}
+                            </button>
+                            {tableTab === 'handled' && (
+                              <Confirm
+                                content={
+                                  <div style={{ textAlign: 'left' }}>
+                                    确定要删除吗
+                                  </div>
+                                }
+                                placement="topRight"
+                                onOk={() => {
+                                  deleteGroupItem(im);
+                                }}
+                              >
+                                <button className="small msw_mock_btn msw_mock_delete_btn">
+                                  删除
+                                </button>
+                              </Confirm>
+                            )}
+                            {tableTab === 'handled' && (
+                              <button
+                                className="small msw_mock_btn msw_mock_canCelHandler_btn"
+                                onClick={() => {
+                                  changeGroupItemStatus(im, false);
+                                }}
+                              >
+                                取消拦截
+                              </button>
+                            )}
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              {paginationMock.length > 1 && (
+                <div className="msw_pagination_wrap">
+                  <button
+                    onClick={() => {
+                      pageSize > 1 && setPageSize(page => page - 1);
+                    }}
+                    disabled={pageSize === 1}
+                    className="small"
+                  >
+                    上一页
+                  </button>
+                  <button
+                    onClick={() => {
+                      pageSize < paginationMock?.length &&
+                        setPageSize(page => page + 1);
+                    }}
+                    disabled={
+                      !(
+                        !!paginationMock?.length &&
+                        pageSize < paginationMock?.length
+                      )
+                    }
+                    className="small"
+                  >
+                    下一页
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
           {currentEditGroupRequest && (
             <AddMockTextArea
               key={
