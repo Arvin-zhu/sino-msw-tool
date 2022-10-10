19355175b4a9988c446d4e2a6a798be1b4683349
arvin.zhu <964505072@qq.com>
2022-10-10 16:17:13 +0800

    refactor: 增加覆盖率要求

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,14 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub',
   },
   testEnvironment: 'jsdom',
-  coveragePathIgnorePatterns: ['/swaggerParseMock/', 'MswUi.tsx'],
+  coveragePathIgnorePatterns: ['/swaggerParseMock/', 'MswUi.tsx', 'upload.tsx', 'drag.tsx'],
+  collectCoverage: true, // 是否显示覆盖率报告
+  coverageThreshold: {
+    global: {
+      statements: 70, // 保证每个语句执行率
+      functions: 70, // 保证函数执行率
+      lines: 70, // 保证行数执行率
+      branches: 60, // 保证分支执行率
+    },
+  },
 };
