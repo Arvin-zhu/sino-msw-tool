c8e62dd8e8f0e77945b8d792175794e74e444034
arvin.zhu <964505072@qq.com>
2022-10-14 16:07:20 +0800

    fix: 排除对引用的项目打包体积影响

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,48 +1,47 @@
-import url from 'rollup-plugin-url';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import url from 'rollup-plugin-url';
 export default {
-    target: 'browser',
-    entry: 'src/index.ts',
-    esm: 'rollup',
-    cjs: 'rollup',
-    runtimeHelpers: true,
-    doc: {
-        themeConfig: { mode: 'light' },
-        base: '/',
-        menu: []
-    },
-    extractCSS: false,
-    lessInRollupMode: {
-        modifyVars: {},
-        javascriptEnabled: true,
-    },
-    externalsExclude: ['mobx', 'mobx-react'],
-    extraRollupPlugins: [
-        [
-            '@babel/plugin-transform-runtime',
-            {
-                corejs: 3,
-            },
-        ],
-        [
-            'babel-plugin-file-loader',
-            {
-                name: '[hash].[ext]',
-                extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
-                context: '',
-                limit: 30 * 1024,
-            },
-        ],
-        [
-            url({
-                limit: 10 * 1024,
-                emitFiles: true
-                })
-        ],
-        [
-            resolve(),
-            commonjs()
-        ]
-    ]
+  target: 'browser',
+  entry: ['src/index.tsx', 'src/MswUi.tsx'],
+  esm: 'rollup',
+  runtimeHelpers: true,
+  doc: {
+    themeConfig: { mode: 'light' },
+    base: '/',
+    menu: [],
+  },
+  extractCSS: false,
+  lessInRollupMode: {
+    modifyVars: {},
+    javascriptEnabled: true,
+  },
+  externalsExclude: ['mobx', 'mobx-react'],
+  replace: {
+    [`require('./MswUi').MswUi`]: `require('./MswUi.esm.js').MswUi`,
+  },
+  extraRollupPlugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3,
+      },
+    ],
+    [
+      'babel-plugin-file-loader',
+      {
+        name: '[hash].[ext]',
+        extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
+        context: '',
+        limit: 30 * 1024,
+      },
+    ],
+    [
+      url({
+        limit: 10 * 1024,
+        emitFiles: true,
+      }),
+    ],
+    [resolve(), commonjs()],
+  ],
 };
